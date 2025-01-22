5665b7ab19c99794f29db9a74de8c3589b628c78
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-01-22 17:42:15 -0300

    Página de erro

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,12 +3,18 @@ import Titulo from 'components/Titulo';
 import styles from './Player.module.css';
 import { useParams } from 'react-router-dom';
 import videos from 'json/db.json';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 function Player() {
     const parametros = useParams();
+    
     const video = videos.find((video) => {
         return video.id === Number(parametros.id);
     })
+
+    if (!video) {
+        return <NaoEncontrada/>
+    }
 
     return (
         <>
