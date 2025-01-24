b2008be08e1d963f5fa82517db7d1e4e9eb37007
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-01-24 17:38:00 -0300

    Fetch com parâmetros

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -2,15 +2,20 @@ import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
 import styles from './Player.module.css';
 import { useParams } from 'react-router-dom';
-import videos from 'json/db.json';
 import NaoEncontrada from 'pages/NaoEncontrada';
+import { useEffect, useState } from 'react';
 
 function Player() {
+    const [video, setVideo] = useState([]);
     const parametros = useParams();
-    
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id);
-    })
+
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/rogerdgela/cinetag-api/videos?id=${parametros.id}`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideo(...dados)
+        })
+    }, [])
 
     if (!video) {
         return <NaoEncontrada/>
